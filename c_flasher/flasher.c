b743d7ea5b98a4f57100d69507d365b1d4624908
Shiva Rajagopal <shiva.rajagopal@outlook.com>
2020-04-12 15:38:01 -0700

    Create stub flasher commands
    Signed-off-by: Shiva Rajagopal <shiva.rajagopal@outlook.com>

--- c_flasher/flasher.c
+++ c_flasher/flasher.c
@@ -3,12 +3,36 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <assert.h>
 
+#include "flasher.h"
 /* @brief
  * Initialize the flasher utility, including any
  * storage that will be needed for flashing
  */
 int flasher_init(void)
+{
+  printf("Initialize flash storage...");
+  return 0;
+}
+
+int flasher_format(void)
+{
+  return 0;
+}
+
+int flasher_write(void)
+{
+  return 0;
+}
+
+int flasher_read(void)
+{
+  return 0;
+}
+
+int flasher_verify(void)
 {
   return 0;
 }
@@ -18,5 +42,24 @@ int flasher_init(void)
  */
 int flasher_run(uint32_t command)
 {
+  enum memory_commands flasher_command = (enum memory_commands) command;
+  int return_code = 0;
+  switch(command)
+  {
+    case memory_format:
+      flasher_format();
+      break;
+    case memory_write:
+      flasher_write();
+      break;
+    case memory_read:
+      flasher_read();
+      break;
+    case memory_verify:
+      flasher_verify();
+      break;
+    default:
+      assert(false);
+  }
   return 0;
 }
--- c_flasher/flasher.h
+++ c_flasher/flasher.h
@@ -4,6 +4,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+enum memory_commands {
+  memory_format = 0x00000000,
+  memory_write  = 0x00000001,
+  memory_read   = 0x00000002,
+  memory_verify = 0x00000003,
+};
+
 /* @brief
  * Initialize the flasher utility, including any
  * storage that will be needed for flashing
