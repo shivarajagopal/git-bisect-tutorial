51ed84b68eac58cc6ca18e71cbac3ad2fd8ea81b
Shiva Rajagopal <shiva.rajagopal@outlook.com>
2020-04-12 18:27:42 -0700

    Comment main.c
    Signed-off-by: Shiva Rajagopal <shiva.rajagopal@outlook.com>

--- c_flasher/main.c
+++ c_flasher/main.c
@@ -11,11 +11,13 @@
 
 volatile uint32_t * command_reg = (volatile uint32_t *)COMMAND_ADDR;
 
+// Get the current value of the command memory location
 uint32_t get_command(void)
 {
   return *(command_reg);
 }
 
+// Set the command memory location to a given value
 void set_command(int32_t val)
 {
   *(command_reg) = val;
@@ -28,22 +30,24 @@ int main(int32_t argc, char **argv)
 
   assert(init_return_code == 0);
 
-  printf("Waiting for command\n");
-  uint32_t command = NO_COMMAND_MAGIC;
+
+  // Infinite loop waiting for a command
   while (true)
   {
-    command = get_command();
-    if (command != NO_COMMAND_MAGIC)
-    {
-      break;
-    }
+    uint32_t command = NO_COMMAND_MAGIC;
+    
+    // Wait for a command
+    while (get_command() == NO_COMMAND_MAGIC);
+  
+    // Run the command
+    int flasher_return_code = flasher_run(get_command());
+
+    // Set the return code into the return code memory location
+    *(volatile int*) COMMAND_RET_CODE_ADDR = flasher_return_code;
+
+    // Reset the command register
+    set_command(NO_COMMAND_MAGIC);
   }
-  // Run the command
-  int flasher_return_code = flasher_run(command);
-  // Set the return code into the return code memory location
-  *(volatile int*) COMMAND_RET_CODE_ADDR = flasher_return_code;
-  // Reset the command register
-  set_command(NO_COMMAND_MAGIC);
   return 0;
 }
 
